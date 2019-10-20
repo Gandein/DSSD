0d343a1a7815feb9b87e9cc513a2e8fb30275b91
Mariano <mazza.mariano@hotmail.com>
2019-10-20 14:31:08 -0300

    ultimas mejoras al form

--- Forms DSSD/js/formDinamico.js
+++ Forms DSSD/js/formDinamico.js
@@ -12,13 +12,14 @@ function addInput(divName){
           inputParticipante.id="nombreParticipante" + (counter + 1);
           inputParticipante.type="text";
           inputParticipante.name="losNombres[]";
-          inputParticipante.className="form-control form-group col-md-6";
+          inputParticipante.className="form-control";
           inputParticipante.placeholder="Nombre del participante";
           newdiv.innerHTML = "Nombre Participante " + (counter + 1);
           newdiv.appendChild(inputParticipante);
           //var idInput="nombreParticipante" + (counter + 1);
           //newdiv.innerHTML = "Nombre Participante " + (counter + 1) + " <input type='text' name='losNombres[]' class='form-control form-group col-md-6' id={idInput} placeholder='Nombre del participante'>";
-          newdiv2.innerHTML = "Rol " + (counter + 1) + "<select name='losRoles[]' class='form-control form-group col-md-6' id='nombreParticipante1'><option>Interno</option><option>Abogado</option><option>Juez</option><option>Procurador</option></select>";
+          newdiv2.className= "form-group col-md-6";
+          newdiv2.innerHTML = "Rol " + (counter + 1) + "<select name='losRoles[]' class='form-control' id='nombreParticipante1'><option>Interno</option><option>Abogado</option><option>Juez</option><option>Procurador</option></select>";
           document.getElementById(divName).appendChild(newdiv);
           document.getElementById(divName).appendChild(newdiv2);
           counter++;
